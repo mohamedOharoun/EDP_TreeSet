82fdc989e5ebc30322499d945f582bc4a2a722e6
TheTatsujin <luis.martin119@alu.ulpgc.es>
2024-04-04 23:32:51 +0100

    Clase Nodo, Apartado de recursos

--- TODO.md
+++ TODO.md
@@ -4,3 +4,8 @@
     - unittest
     - add
     - Red-black implementation
+
+# Resources
+
+  - Implementacion Red-Black Tree
+    - [Introduction to Algorithms] (https://mcube.lab.nycu.edu.tw/~cfung/docs/books/cormen2001algorithms.pdf)
--- TreeSet.py
+++ TreeSet.py
@@ -1,5 +1,29 @@
 # Red-Black Tree
+from enum import Enum
+
+
+class Color(Enum):
+    RED = 0
+    BLACK = 1
+
 
 class TreeSet:
-    def __init__(self):
+    class Node:
+        def __init__(self, key=None, color=Color.RED, left=None, right=None, parent=None):
+            self.color = color
+            self.left = left
+            self.right = right
+            self.key = key
+            self.parent = parent
+
+    def __init__(self, type, collection=None):
+        if type is None:
+            raise Exception
+        self._type = type
+        self._root = None
+
+
+    def add(self, new_key):
         pass
+
+        
